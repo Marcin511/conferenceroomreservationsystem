55a5b31dac688fc3c8bf99118e32fce7c89c9205
Marcin <szpak111@gmail.com>
2022-07-30 12:24:53 +0200

    Init ConferenceRoomReservationSystem project

---
+++ src/main/java/finalProject/ConferanceRoomReservationSystem/ConferanceRoomReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package finalProject.ConferanceRoomReservationSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferanceRoomReservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferanceRoomReservationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/finalProject/ConferanceRoomReservationSystem/ConferanceRoomReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package finalProject.ConferanceRoomReservationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferanceRoomReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
